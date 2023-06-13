636b3f7ed1031f55c2b6aa7f861566c711b0e271
BolleTito335409 <chemichaeldekraker@gmail.com>
2023-06-13 14:39:24 +0200

    ook link href veranderd voor styling

--- app/views/Rijlessen/overzichtRijlessen.php
+++ app/views/Rijlessen/overzichtRijlessen.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,1,200" />
-    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+    <link rel="stylesheet" href="/public/css/style.css">
     <title>Overzicht Rijlessen</title>
 </head>
 <body>
